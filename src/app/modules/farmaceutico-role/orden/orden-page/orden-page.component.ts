d06b0da47bf6b8c349195dd0b12dfe3489dc0854
MellanyApolino <apolinomellany@gmail.com>
2025-02-08 00:52:34 -0500

    Creando componentes de farmaceutico

---
+++ src/app/modules/farmaceutico-role/orden/orden-page/orden-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orden-page',
+  imports: [],
+  templateUrl: './orden-page.component.html',
+})
+export class OrdenPageComponent {
+
+}
