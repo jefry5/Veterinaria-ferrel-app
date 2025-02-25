626ee14d4278a1fc601cdf8cbe8233b06ab939d1
Lacerqwer <71660930+Lacerqwer@users.noreply.github.com>
2025-02-25 15:21:11 -0500

    intemedio

--- src/app/core/services/recepcionista-role/pagos/pagos.service.ts
+++ src/app/core/services/recepcionista-role/pagos/pagos.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { OrdenPago } from '@core/models/orden-pago.model';
+import { TokenService } from '@core/services/JWT/token.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagosService {
+  private BASE_URL: string = '';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+  ) {
+    this.BASE_URL = environment.url;
+  }
+
+  getListaPagosPendientes(){
+    return this.http.get<OrdenPago>(`${this.BASE_URL}/api/ordenes/pendientes`);
+  }
+
+  postConfirmar(idCliente: number){
+    return this.http.post(`${this.BASE_URL}/carrito/confirmar/${idCliente}`, {});
+  }
+
+  postSeleccionar(idOrden: number){
+    return this.http.post(`${this.BASE_URL}/carrito/agregar/${idOrden}`, {});
+  }
+  postAgregarConsulta(precioConsulta: number){
+    return this.http.post(`${this.BASE_URL}/carrito/agregar-consulta`, precioConsulta);
+  }
+  getListaCarrito(){
+    return this.http.get<any>(`${this.BASE_URL}/carrito/listar`);
+  }
+  deleteOrdenPago(idOrden: number){
+    return this.http.delete(`${this.BASE_URL}/carrito/quitar/${idOrden}`);
+  }
+  deleteConsulta(){
+    return this.http.delete(`${this.BASE_URL}/carrito/quitar-consulta`);
+  }
 }
--- src/app/modules/recepcionista-role/pagos/pagos-pages/pagos-pages.component.ts
+++ src/app/modules/recepcionista-role/pagos/pagos-pages/pagos-pages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
@@ -7,6 +7,10 @@ import { ButtonGroupModule } from 'primeng/buttongroup';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
 import { DividerModule } from 'primeng/divider';
+import { PagosService } from '@core/services/recepcionista-role/pagos/pagos.service';
+import { MessageService } from 'primeng/api';
+import { InputNumber } from 'primeng/inputnumber';
+import { AutoComplete } from 'primeng/autocomplete';
 
 
 @Component({
@@ -19,16 +23,31 @@ import { DividerModule } from 'primeng/divider';
     ButtonGroupModule,
     TableModule,
     FormsModule,
-    DividerModule
+    DividerModule,
+    InputNumber,
+    AutoComplete
 
   ],
   templateUrl: './pagos-pages.component.html',
   styleUrl: './pagos-pages.component.css',
 })
-export class PagosPagesComponent {
-  dni: number | undefined
+export class PagosPagesComponent implements OnInit{
+  consulta: number | undefined
   id: number | undefined
   monto: number | undefined
-
+  items: any[] = [];
   products: any[] = [];
+
+  constructor(private pagosService: PagosService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  buscar(){
+    this.items =[12323]
+  }
+
 }
